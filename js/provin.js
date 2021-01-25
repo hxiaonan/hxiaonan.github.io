8bb13f00c343ecedacde62939bf91d938d665726
root <root@DESKTOP-DPNSHF9.localdomain>
2021-01-25 21:38:37 +0800

    Site updated: 2021-01-25 21:38:33

---
+++ js/provin.js
@@ -0,0 +1,5 @@
+document.writeln("<li><a href=\'../channel/cctv1.html\' title=\'CCTV1\'>CCTV1</a></li>	");
+document.writeln("<li><a href=\'../channel/cctv5.html\' title=\'CCTV5\'>CCTV5</a></li>");
+document.writeln("<li><a href=\'../channel/cctv5+.html\' title=\'CCTV5+\'>CCTV5+</a></li>");
+document.writeln("<li><a href=\'../channel/cctv6.html\' title=\'CCTV6\'>CCTV6</a></li>");
+document.writeln("<li><a href=\'../channel/cctv8.html\' title=\'CCTV8\'>CCTV8</a></li>");
