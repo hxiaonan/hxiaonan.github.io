c356dc4b672afc8021661099c7fc0844d797282f
root <root@DESKTOP-DPNSHF9.localdomain>
2021-02-07 22:25:54 +0800

    Site updated: 2021-02-07 22:25:50

---
+++ js/provinf.js
@@ -0,0 +1,3 @@
+document.writeln("<li><a href=\'../channel/fenghuangweishi.html\' title=\'凤凰卫视\'>凤凰卫视</a></li>	");
+document.writeln("<li><a href=\'../channel/fenghuangweishizx.html\' title=\'凤凰卫视资讯台\'>凤凰卫视资讯台</a></li>");
+document.writeln("<li><a href=\'../channel/fenghuangweishixg.html\' title=\'凤凰卫视香港台\'>凤凰卫视香港台</a></li>");
