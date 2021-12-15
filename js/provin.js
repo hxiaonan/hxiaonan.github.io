5a8db05441b9d8a48e5c8e96dac21ce0689624de
root <root@DESKTOP-DPNSHF9.localdomain>
2021-12-15 20:53:47 +0800

    Site updated: 2021-12-15 20:53:32

--- js/provin.js
+++ js/provin.js
@@ -1,5 +1,5 @@
-document.writeln("<li><a href=\'../channel/cctv1.html\' title=\'CCTV1\'>CCTV1</a></li>	");
-document.writeln("<li><a href=\'../channel/cctv5.html\' title=\'CCTV5\'>CCTV5</a></li>");
-document.writeln("<li><a href=\'../channel/cctv5+.html\' title=\'CCTV5+\'>CCTV5+</a></li>");
-document.writeln("<li><a href=\'../channel/cctv6.html\' title=\'CCTV6\'>CCTV6</a></li>");
-document.writeln("<li><a href=\'../channel/cctv8.html\' title=\'CCTV8\'>CCTV8</a></li>");
+document.writeln("<li><a href=\'../channel/cctv1.html\' title=\'香港卫视'>香港卫视</a></li>	");
+document.writeln("<li><a href=\'../channel/cctv5.html\' title=\'奥视卫星\'>奥视卫星</a></li>");
+document.writeln("<li><a href=\'../channel/cctv5+.html\' title=\'潮安综艺\'>潮安综艺</a></li>");
+document.writeln("<li><a href=\'../channel/cctv6.html\' title=\'点掌财经\'>点掌财经</a></li>");
+document.writeln("<li><a href=\'../channel/cctv8.html\' title=\'中国气象\'>中国气象</a></li>");
